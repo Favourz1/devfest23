eca38ed72857aef6316509de3f7f894509d6f87d
Favour Okoh <okohfavour91@gmail.com>
2023-10-13 19:55:51 +0100

    Chore: New Sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import devfestLogo from "./assets/images/devfest-logo.png"
 import devfest23Logo from "./assets/images/devfest23-logo.png"
 import globeIllustration from "./assets/images/globe-illustration.png"
 import './App.css'
+import { HandShakeSvg, LaptopSvg, PeopleSvg, PersonSvg } from "./assets/svgs"
 
 function App() {
 
@@ -40,6 +41,51 @@ function App() {
             </div>
           </div>
         </section>
+        <section className="py-8 bg-bgWhite">
+          <div className="container mx-auto px-3">
+            <div className="flex flex-col gap-12 md:flex-row">
+              <div className="flex flex-col items-center w-full md:w-[40%] pt-10 pb-5">
+                <h2 className="text-4xl font-bold font-inter mb-8">About</h2>
+                <p className="text-2xl mb-14">Whether you are a beginner or an experienced developer, GDG Warri welcomes you. Join our community and be a part of the vibrant tech scene in our city. Together, let&apos;s learn, grow, and make a difference in the world of technology.</p>
+                <a href="group/link" className="flex self-start items-center text-xl font-nunito font-medium underline">
+                  <span>Learn More</span>
+                  <span className="ml-1 group-hover/link:translate-x-[5px]">
+                    <svg className="group-hover:translate-x-2" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 23 20" fill="none">
+                      <path d="M20.9549 3.58672C18.4426 3.92872 15.1947 4.3302 13.3474 3.9151C11.5001 3.49999 7.51714 1.94417 8.50019 1.99996M19.5935 14.7973C18.6326 13.8313 18.4993 11.8806 18.4993 10.4989C18.4993 9.11724 18.6972 5.28905 21.013 3.45998C15.047 9.22708 7.90737 12.5798 1.74265 17.8889" stroke="#4285F4" strokeWidth="3" strokeLinecap="round" />
+                    </svg>
+                  </span>
+                </a>
+              </div>
+              <div className="w-full md:w-[60%] flex items-center relative">
+                about pic
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="py-8 bg-bgWhite">
+          <div className="container mx-auto px-3">
+            <div className="relative grid grid-cols-2 gap-12 gap-y-44">
+              <img className="absolute w-32 right-[6%] top-[-35%] rotate-[35.4deg] z-2" src={globeIllustration} alt="Globe" />
+              <div className="flex justify-center items-center text-2xl text-[#514D4D] font-bold">
+                <PeopleSvg />
+                <span className="ml-4">562 Members</span>
+              </div>
+              <div className="flex justify-center items-center text-2xl text-[#514D4D] font-bold">
+                <PersonSvg />
+                <span className="ml-4">Personal Development</span>
+              </div>
+              <div className="flex justify-center items-center text-2xl text-[#514D4D] font-bold">
+                <LaptopSvg />
+                <span className="ml-4">3+ Annual meetups</span>
+              </div>
+              <div className="flex justify-center items-center text-2xl text-[#514D4D] font-bold">
+                <HandShakeSvg />
+                <span className="ml-4">Team Collaboration</span>
+              </div>
+            </div>
+          </div>
+        </section>
+
       </main>
     </>
   )
---
+++ src/assets/svgs/index.js
@@ -0,0 +1,4 @@
+export { default as PeopleSvg } from "./PeopleSvg"
+export { default as PersonSvg } from "./PersonSvg"
+export { default as LaptopSvg } from "./LaptopSvg"
+export { default as HandShakeSvg } from "./HandShakeSvg"
