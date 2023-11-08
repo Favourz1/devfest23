653708f6bfcff01086f2b68a97bc64b12a32f7df
Favour Okoh <okohfavour91@gmail.com>
2023-11-08 09:45:34 +0100

    Fix: Single Speaker view

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,3 +1,4 @@
+import SPEAKERS_DATA from "../data/speakerData"
 import SingleSpeaker from "./SingleSpeaker"
 
 const Speakers = () => {
@@ -5,8 +6,16 @@ const Speakers = () => {
         <section className="py-14">
             <div className="container mx-auto px-3">
                 <div className="text-4xl font-bold font-inter mb-10 text-center">Speakers</div>
-                <div className="relative grid grid-cols-1 md:grid-cols-2 gap-[80px] w-full">
-                    <SingleSpeaker />
+                <div className="relative grid grid-cols-1 md:grid-cols-2 gap-[80px] w-full justify-items-center items-center">
+                    {SPEAKERS_DATA.map(item => (
+                        <SingleSpeaker
+                            key={item.name}
+                            name={item.name}
+                            bgImage={item.bgImage}
+                            organization={item.organization}
+                            role={item.role}
+                        />
+                    ))}
                 </div>
             </div>
         </section>
